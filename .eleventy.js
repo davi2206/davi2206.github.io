d70d688d90b9e4d66c9c5e2789514e862496ad45
David <dmkolind@hotmail.com>
2025-12-02 22:29:57 +0100

    Added RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -5,25 +5,23 @@ const customCollections = require('./eleventy-config/collections');
 const collections = require('./eleventy-config/collections');
 
 const futurePost = require("eleventy-plugin-future-post");
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(futurePost);
-    
+
     // Register custom JS files
     filters(eleventyConfig);
     passThrough(eleventyConfig);
     customCollections(eleventyConfig);
 
+    eleventyConfig.addPlugin(rssPlugin);
+
     // Exclude future posts
     eleventyConfig.addGlobalData("eleventyComputed.permalink", (data) => {
-        if(!data) return;
-        // Always output on dev (serve, watch)
-        //if (process.env.BUILD_DRAFTS) return data.permalink;
-        // Do not build files with a future date
         if (data.date && new Date(data.date) > new Date()) return false;
         return data.permalink;
     });
-
 
     return {
         dir: {
