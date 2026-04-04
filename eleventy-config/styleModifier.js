175b03553ae18ed4ca64b7824f1284f1691d3d8f
David <github@northlore.dk>
2026-04-04 18:24:25 +0200

    Better styling

--- eleventy-config/styleModifier.js
+++ eleventy-config/styleModifier.js
@@ -4,67 +4,58 @@ module.exports = () => {
   const month = now.getMonth() + 1; // 1-12
   const day = now.getDate(); // 1-31 Apparently months are 0-indexed, but dates are not..
 
-  // Early January
+  var season = 'default';
+  var bgImage = '/assets/images/bg.jpg';
+  var overlayDeg = '0deg';
+  var overlayColor1 = 'rgba(0, 0, 0, 0.1)';
+  var overlayColor2 = 'rgba(0, 0, 0, 0.1)';
+  var postFontFamily = 'Inter Variable, Inter, -apple-system, BlinkMacSystemFont, sans-serif';
+  var postFontSize = '1em';
+
+  // Early January - New Year
   if (month == 1 && day < 10) {
-    return {
-      season: 'new_year',
-      bgImage: '/assets/images/bg_newyear.png',
-      overlayDeg: '0deg',
-      overlayColor1: 'rgba(0, 0, 0, 0.2)',
-      overlayColor2: 'rgba(0, 0, 0, 0.1)'
-    };
+    season = 'new_year';
+    bgImage = '/assets/images/bg_newyear.png';
+    overlayColor1 = 'rgba(0, 0, 0, 0.2)';
   }
 
-  // Late October
-  if (month == 10 && day > 25) {
-    return {
-      season: 'halloween',
-      bgImage: '/assets/images/bg_halloween.png',
-      overlayDeg: '0deg',
-      overlayColor1: 'rgba(0, 0, 0, 0)',
-      overlayColor2: 'rgba(0, 0, 0, 0)'
-    };
-  }
-
-  // October
+  // October - Full Moon and Halloween
   if (month == 10) {
-    return {
-      season: 'halloween',
-      bgImage: '/assets/images/bg_fullmoon.png',
-      overlayDeg: '0deg',
-      overlayColor1: 'rgba(0, 0, 0, 0)',
-      overlayColor2: 'rgba(0, 0, 0, 0)'
-    };
+    season = 'halloween';
+    overlayColor1 = 'rgba(0, 0, 0, 0)';
+    overlayColor2 = 'rgba(0, 0, 0, 0)';
+    
+    if(day > 25) {
+      bgImage = '/assets/images/bg_halloween.png';
+    }
+    else {
+      bgImage = '/assets/images/bg_fullmoon.png';
+    }
   }
+  
+  // December - Christmas and New Year
+  if (month == 12) {
+    overlayColor1 = 'rgba(0, 0, 0, 0.5)';
+    overlayColor2 = 'rgba(0, 0, 0, 0.1)';
 
-  // December
-  if (month == 12 && day < 28) {
-    return {
-      season: 'christmas',
-      bgImage: '/assets/images/bg_christmas.png',
-      overlayDeg: '0deg',
-      overlayColor1: 'rgba(0, 0, 0, 0.5)',
-      overlayColor2: 'rgba(0, 0, 0, 0.1)'
-    };
-  }
-
-  // End of December
-  if (month == 12 && day >= 28) {
-    return {
-      season: 'new_year',
-      bgImage: '/assets/images/bg_newyear.png',
-      overlayDeg: '0deg',
-      overlayColor1: 'rgba(0, 0, 0, 0.5)',
-      overlayColor2: 'rgba(0, 0, 0, 0.1)'
-    };
+    if (day < 27) {
+      season = 'christmas';
+      bgImage = '/assets/images/bg_christmas.png';
+    }
+    else {
+      season = 'new_year';
+      bgImage = '/assets/images/bg_newyear.png';
+    }
   }
 
   // Rest of the year
   return {
-    season: 'default',
-    bgImage: '/assets/images/bg.jpg',
-    overlayDeg: '0deg',
-    overlayColor1: 'rgba(0, 0, 0, 0.1)',
-    overlayColor2: 'rgba(0, 0, 0, 0.1)'
+    season: season,
+    bgImage: bgImage,
+    overlayDeg: overlayDeg,
+    overlayColor1: overlayColor1,
+    overlayColor2: overlayColor2,
+    postFontFamily: postFontFamily,
+    postFontSize: postFontSize
   };
 };
