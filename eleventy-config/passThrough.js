0d0260ae08cee14f7fcf4755f5fb86e85d11c565
David <github@northlore.dk>
2026-04-14 17:26:13 +0200

    Added robots.txt

--- eleventy-config/passThrough.js
+++ eleventy-config/passThrough.js
@@ -4,5 +4,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
 };
