877d17b382717f2a9de45efb54914b94c0c72af1
David <github@northlore.dk>
2026-04-23 22:55:21 +0200

    Added "money" shortcode

--- eleventy-config/shortcodes.js
+++ eleventy-config/shortcodes.js
@@ -5,6 +5,32 @@ module.exports = function (eleventyConfig) {
         return `<span id="${id}">${content}</span>`;
     });
 
+    eleventyConfig.addShortcode("money", function(amount) {
+        const userLocale = navigator.languages?.[0] || navigator.language || 'en-US';
+        const formatter = new Intl.NumberFormat(userLocale, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        });
+
+        const formatterSmall = new Intl.NumberFormat(userLocale, {
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        });
+
+        let eur = formatter.format(amount/7.5);
+        let gbp = formatter.format(amount/8.6);
+        let usd = formatter.format(amount/6.4);
+
+        return `<span class="tooltip">Dkk ${formatterSmall.format(amount)},-
+                    <span class="tooltip-text">
+                        Approximate:<br>
+                        € ${eur}<br>
+                        £ ${gbp}<br>
+                        $ ${usd}
+                    </span>
+                </span>`;
+    });
+
     // Social media links
     eleventyConfig.addShortcode("twitter", function() {
         return `<a href="${site.twitter}" target="_blank">X</a>`;
